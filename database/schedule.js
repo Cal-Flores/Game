5db205a0d41a876f604349693e9005fff712d2ad
calebflores <calebflores1388@gmail.com>
2024-10-04 15:42:01 -0500

    curse auto buffd

--- database/schedule.js
+++ database/schedule.js
@@ -4,3 +4,8 @@ week_1 = [
     'Penn State vs Iowa',
     'Ohio State vs Michigan'
 ]
+
+function statCalc(grade) {
+    let stat = (1020 * grade) / 4
+    console.log(`grade ${grade} is ${stat} per stat`)
+}
