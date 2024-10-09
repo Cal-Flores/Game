6eda7ae090977b2765cd8df4934de1645093f360
calebflores <calebflores1388@gmail.com>
2024-10-09 17:01:35 -0500

    potency bug fix

--- database/database.js
+++ database/database.js
@@ -101,7 +101,7 @@ const database = [
         accuracy: 9,
         evasion: 5,
         luck: 7,
-        stun: .10,
+        stun: .06,
         //GENJUTSU
         genjutsu: 8,
         gen_power: 25,
@@ -140,5 +140,137 @@ const database = [
         buff_acc: 30,
         buff2_acc: 45,
         image: 'https://64.media.tumblr.com/11d79e538d170ebc91403fa7825be14a/ac5ce7753d2dffd1-7d/s1280x1920/5e3752a4fc826febaef45197801b139f48d244de.jpg'
-    }
+    },
+    {
+        name: 'Nobunaga',
+        nen_type: 'Transmuter',
+        element: 'Dark',
+        elm: dark,
+        hp: 3152,
+        //PHYSICAL
+        attack: 185,
+        defense: 140,
+        critical: .35,
+        strength: 2,
+        //MAGIC
+        elm_attack: 157,
+        elm_defense: 232,
+        elm_efficiency: .12,
+        potency: .16,
+        //WEAPON
+        weapon: 270,
+        shield: 164,
+        curse_crit: .17,
+        weaponCurse: 'Purity',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 9,
+        evasion: 5,
+        luck: 3,
+        stun: .08,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 10,
+        willpower: 7,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 49,
+        negative_energy: 18,
+        cursed_tech: 'Blade of the Right Hand - 90% Critical rate',
+        curse_amount: .55,
+        curse_duration: 8,
+        curse_cost: 21,
+        //DOMAIN
+        dom_purity: 3,
+        dom_lethal: 85,
+        dom_duration: 6,
+        dom_cost: 34,
+        //NINJUTSU
+        ninjutsu2: 'En-Strike - Decrease Attack by 5%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 165,
+        nin2amount: .05,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Iaidō-Master - Decrease Magic by 5%",
+        nin_cost: 15,
+        ninjutsu_pow: 165,
+        ninamount: .05,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Double Domain Purity',
+        buff2: 'Decrease Will Power by 20%',
+        buff_acc: 60,
+        buff2_acc: 60,
+        image: 'https://static.wikia.nocookie.net/hunterxhunter/images/4/46/HxH2011_EP50_Nobunaga_Portrait.png/revision/latest?cb=20230122183803'
+    },
+    {
+        name: 'Mikasa',
+        nen_type: 'Enhancer',
+        element: 'Lightning',
+        elm: lightning,
+        hp: 2971,
+        //PHYSICAL
+        attack: 228,
+        defense: 173,
+        critical: .38,
+        strength: 1.4,
+        //MAGIC
+        elm_attack: 241,
+        elm_defense: 126,
+        elm_efficiency: 1.2,
+        potency: .20,
+        //WEAPON
+        weapon: 245,
+        shield: 169,
+        curse_crit: .20,
+        weaponCurse: 'Defense',
+        weapon_amount: 25,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 8,
+        luck: 6,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 5,
+        gen_power: 15,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 2,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 27,
+        negative_energy: 14,
+        cursed_tech: 'Projection-Sorcery',
+        curse_amount: 8,
+        curse_duration: 8,
+        curse_cost: 17,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Thunder-Spear - Increase Effieciency by 35%',
+        ninJutsu2cost: 13,
+        ninJutsu2Dmg: 245,
+        nin2amount: .35,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Awakening - Increase Critical by 80%",
+        nin_cost: 26,
+        ninjutsu_pow: 0,
+        ninamount: .80,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Shield by 25%',
+        buff2: 'Decrease Accuracy by 2',
+        buff_acc: 70,
+        buff2_acc: 70,
+        image: 'https://preview.redd.it/what-do-you-dislike-about-mikasas-character-v0-s42udm18c6bb1.jpg?auto=webp&s=e7c072a57d924d5aa9bd83dbae93ed7bc93c160f'
+    },
 ]
