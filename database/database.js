959027fd8ef13721b7deeb4aa4ecb9803aa826a1
calebflores <calebflores1388@gmail.com>
2024-10-16 13:30:29 -0500

    adult gon added

--- database/database.js
+++ database/database.js
@@ -1851,7 +1851,7 @@ const database = [
         nin2amount: .10,
         ninEnhance: false,
         //ULTIMATE
-        ninjutsu: "Jajanken - Increase Strength by 1 | Reduce Recovery by 3",
+        ninjutsu: "Jajanken - Stengthen Jajanken by 200 | Reduce Recovery by 2",
         nin_cost: 20,
         ninjutsu_pow: 350,
         ninamount: 0,
@@ -2068,10 +2068,10 @@ const database = [
         elm: earth,
         hp: 2992,
         //PHYSICAL
-        attack: 238,
+        attack: 231,
         defense: 188,
         critical: .60,
-        strength: 1.35,
+        strength: 1.2,
         //MAGIC
         elm_attack: 218,
         elm_defense: 196,
@@ -2085,7 +2085,7 @@ const database = [
         weapon_amount: 15,
         //SPEED / LUCK
         accuracy: 5,
-        evasion: 5,
+        evasion: 4,
         luck: 2,
         stun: .07,
         //GENJUTSU
@@ -2100,7 +2100,7 @@ const database = [
         cursed_recovery: 15,
         negative_energy: 14,
         cursed_tech: 'Granite-Blast - Deal 300 Damage',
-        curse_amount: 300,
+        curse_amount: 260,
         curse_duration: 1,
         curse_cost: 18,
         //DOMAIN
@@ -2110,7 +2110,7 @@ const database = [
         dom_cost: 30,
         //NINJUTSU
         ninjutsu2: 'C3 - Increase Attack by 5%',
-        ninJutsu2cost: 14,
+        ninJutsu2cost: 15,
         ninJutsu2Dmg: 145,
         nin2amount: .05,
         ninEnhance: true,
@@ -2121,7 +2121,7 @@ const database = [
         ninamount: 20,
         ultEnhance: true,
         //BUFFS
-        buff: 'Increase Evasion by 2',
+        buff: 'Increase Evasion by 3',
         buff2: 'Decrease Purity by 3',
         buff_acc: 60,
         buff2_acc: 60,
@@ -2324,5 +2324,71 @@ const database = [
         buff_acc: 40,
         buff2_acc: 65,
         image: 'https://static.wikia.nocookie.net/hunterxhunter/images/c/cd/Biscuit_Krueger_CA_Portrait.png/revision/latest?cb=20190630140152'
+    },
+    {
+        name: 'Adult Gon',
+        nen_type: 'Enhancer',
+        element: 'Ghost',
+        elm: ghost,
+        hp: 3500,
+        //PHYSICAL
+        attack: 300,
+        defense: 248,
+        critical: .20,
+        strength: 1.8,
+        //MAGIC
+        elm_attack: 131,
+        elm_defense: 201,
+        elm_efficiency: 1.3,
+        potency: .10,
+        //WEAPON
+        weapon: 128,
+        shield: 172,
+        curse_crit: .08,
+        weaponCurse: 'Evasion',
+        weapon_amount: 1,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 8,
+        luck: 4,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 40,
+        willpower: 5,
+        chakra: 0,
+        chakra_rec: 8,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 20,
+        negative_energy: 18,
+        cursed_tech: 'Projection-Sorcery - Double Evasion',
+        curse_amount: 8,
+        curse_duration: 6,
+        curse_cost: 18,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Scissors - Increase Critical by .05',
+        ninJutsu2cost: 16,
+        ninJutsu2Dmg: 150,
+        nin2amount: .05,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Jajanken - Increase Jajanken by 200 | Reduce Recovery by 2",
+        nin_cost: 25,
+        ninjutsu_pow: 400,
+        ninamount: 0,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Curse Recovery by 15',
+        buff2: 'Decrease Curse Recovery by 15',
+        buff_acc: 50,
+        buff2_acc: 50,
+        image: 'https://pbs.twimg.com/profile_images/1397430377996824577/QXzPZoyh_400x400.jpg'
     },
 ]
