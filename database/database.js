6bf942101f8225fc6f0d0ef8e1d70dccadb4cfbf
calebflores <calebflores1388@gmail.com>
2024-10-16 19:36:00 -0500

    netero added

--- database/database.js
+++ database/database.js
@@ -2589,4 +2589,137 @@ const database = [
         buff2_acc: 50,
         image: 'https://static.wikia.nocookie.net/shingekinokyojin/images/a/ae/Attack_Titan_%28Anime%29_character_image_%28Eren_Jaeger%29.png/revision/latest/scale-to-width-down/1000?cb=20170513212951'
     },
+    {
+        name: 'Todo',
+        nen_type: 'Enhancer',
+        element: 'Earth',
+        elm: earth,
+        hp: 3494,
+        //PHYSICAL
+        attack: 264,
+        defense: 230,
+        critical: .30,
+        strength: 1.75,
+        //MAGIC
+        elm_attack: 113,
+        elm_defense: 136,
+        elm_efficiency: 1,
+        potency: .01,
+        //WEAPON
+        weapon: 184,
+        shield: 218,
+        curse_crit: .09,
+        weaponCurse: 'Defense',
+        weapon_amount: 12,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 7,
+        luck: 2,
+        stun: .03,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 3,
+        drain: 6,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 55,
+        negative_energy: 15,
+        cursed_tech: 'Black-Flash - 15/15',
+        curse_amount: 0,
+        curse_duration: 1,
+        curse_cost: 15,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Playful-Cloud - Increase Attack by 6%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 100,
+        nin2amount: .06,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "Vibra-Slap - Increase Curse by 10",
+        nin_cost: 20,
+        ninjutsu_pow: 170,
+        ninamount: 10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Accuracy by 2',
+        buff2: 'Decrease Magic by 10%',
+        buff_acc: 50,
+        buff2_acc: 45,
+        image: 'https://static.wikia.nocookie.net/jujutsu-kaisen/images/e/ef/Aoi_Todo.jpg/revision/latest?cb=20210904074234&path-prefix=pl'
+    },
+    {
+        name: 'Netero',
+        nen_type: 'Enhancer',
+        element: 'Fighter',
+        elm: fighter,
+        hp: 3275,
+        //PHYSICAL
+        attack: 297,
+        defense: 215,
+        critical: .25,
+        strength: 1.5,
+        //MAGIC
+        elm_attack: 130,
+        elm_defense: 205,
+        elm_efficiency: 1,
+        potency: 0.15,
+        //WEAPON
+        weapon: 145,
+        shield: 225,
+        curse_crit: .20,
+        weaponCurse: 'Defense',
+        weapon_amount: 25,
+        //SPEED / LUCK
+        accuracy: 15,
+        evasion: 5,
+        luck: 2,
+        stun: .06,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 3,
+        chakra: 0,
+        chakra_rec: 10,
+        drain: 2,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 25,
+        negative_energy: 15,
+        cursed_tech: 'Ninety-Ninth-Hand - Add 100 to Attack',
+        curse_amount: 100,
+        curse_duration: 6,
+        curse_cost: 25,
+        //DOMAIN
+        dom_purity: 7,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Third-Hand - Increase Evasion by 1',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 185,
+        nin2amount: 1,
+        ninEnhance: true,
+        //ULTIMATE
+        ninjutsu: "First-Hand - Deal 330 Damage",
+        nin_cost: 16,
+        ninjutsu_pow: 330,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Recovery by 2',
+        buff2: 'Zero-Hand - Deal 1000 Damage',
+        buff_acc: 30,
+        buff2_acc: 0,
+        image: 'https://64.media.tumblr.com/b3add61800c26d58b7e6a7627c9b2c00/e1d0c57be4c47966-59/s1280x1920/960f28661bd237e20ba6b5a71a74a9d127f51e9f.jpg'
+    },
+    ///////////////////// END ////////////////////////
 ]
