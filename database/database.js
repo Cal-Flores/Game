cdda655f57a715bff5b87e983432b057b3b29d5f
calebflores <calebflores1388@gmail.com>
2024-11-21 21:01:59 -0600

    mech added

--- database/database.js
+++ database/database.js
@@ -9006,5 +9006,78 @@ const database = [
         buff_acc: 55,
         buff2_acc: 50,
         image: 'https://attachments-cdn-s.coub.com/coub_storage/coub/simple/cw_timeline_pic/2226b923534/9c498a7b4d7133fec88c1/ios_large_1616495997_image.jpg'
-    }
+    },
+
+    {
+        name: 'Mechamaru',
+        nen_type: 'Manipulator',
+        element: 'Holy',
+        elm: holy,
+        hp: 3174,
+        //PHYSICAL
+        attack: 180,
+        defense: 176,
+        critical: .10,
+        strength: 1.41,
+        //MAGIC
+        elm_attack: 134,
+        elm_defense: 148,
+        elm_efficiency: 1.22,
+        potency: .10,
+        //WEAPON
+        weapon: 195,
+        shield: 237,
+        curse_crit: .20,
+        weaponCurse: 'Attack',
+        weapon_amount: 9,
+        //SPIRIT
+        spirit: 255,
+        spirit_def: 224,
+        spirit_crit: .10,
+        spiritCurse: 'Damage',
+        spirit_amount: 65,
+        //SPEED / LUCK
+        accuracy: 7,
+        evasion: 4,
+        luck: 2,
+        stun: .04,
+        //GENJUTSU
+        genjutsu: 3,
+        gen_power: 10,
+        willpower: 4,
+        chakra: 0,
+        chakra_rec: 5,
+        drain: 0,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 55,
+        negative_energy: 25,
+        cursed_tech: 'Sword-Option - Increase Weapon by 55%',
+        curse_amount: .55,
+        curse_duration: 8,
+        curse_cost: 20,
+        //DOMAIN
+        dom_purity: 5,
+        dom_lethal: 300,
+        dom_duration: 4,
+        dom_cost: 55,
+        //NINJUTSU
+        ninjutsu2: 'Albatross - Decrease Talisman by 12%',
+        ninJutsu2cost: 15,
+        ninJutsu2Dmg: 200,
+        nin2amount: .12,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Pigeon-Viola - Sacrife Curse Rec but eleminate Recovery",
+        nin_cost: 30,
+        ninjutsu_pow: 500,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Domain by 5',
+        buff2: 'Decrease Domain by 4',
+        buff_acc: 50,
+        buff2_acc: 60,
+        image: 'https://thicc-uwu.mywaifulist.moe/waifus/kokichi-muta/4yrHUd26dvaZ08bHXkZmQkcWM4fyKQ6OcrjMajpF.jpg?class=thumbnail'
+    },
 ]
