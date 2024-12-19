862f967da59054ffbf74efc875b7d2f3a95cd4be
calebflores <calebflores1388@gmail.com>
2024-12-18 22:42:17 -0600

    blue spirit def fic

--- database/database.js
+++ database/database.js
@@ -2836,6 +2836,78 @@ const database = [
         //image: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.6435-9/117715424_339264554120736_6396455950920254671_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_ohc=-s3eV__-XrgQ7kNvgHGm9UX&_nc_zt=23&_nc_ht=scontent-dfw5-1.xx&_nc_gid=ASWUucZtc60dg1RiUkPUVWP&oh=00_AYA_okgp2TdAMkImBh674qHd8ZR6Lh_wkPygOGxjquEF9A&oe=6749CE72'
     },
     {
+        name: 'Goku',
+        nen_type: 'Enhancer',
+        element: 'Holy',
+        elm: holy,
+        hp: 3332,
+        //PHYSICAL
+        attack: 310,
+        defense: 207,
+        critical: .18,
+        strength: 1.60,
+        //MAGIC
+        elm_attack: 79,
+        elm_defense: 129,
+        elm_efficiency: .11,
+        potency: 1.0,
+        //WEAPON
+        weapon: 82,
+        shield: 218,
+        curse_crit: .20,
+        weaponCurse: 'Luck',
+        weapon_amount: 2,
+        //SPIRIT
+        spirit: 180,
+        spirit_def: 162,
+        spirit_crit: .10,
+        spiritCurse: 'Recovery',
+        spirit_amount: 2,
+        //SPEED / LUCK
+        accuracy: 5,
+        evasion: 7,
+        luck: 2,
+        stun: .03,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 2,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 3,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 15,
+        cursed_tech: 'Kaio-Ken - Increase attack by 50',
+        curse_amount: 50,
+        curse_duration: 6,
+        curse_cost: 30,
+        //DOMAIN
+        dom_purity: 9,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Kamehameha - Lower Curse Recovery',
+        ninJutsu2cost: 16,
+        ninJutsu2Dmg: 265,
+        nin2amount: 15,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Spirit-Bomb - 15x damage recovery goes to 0",
+        nin_cost: 0,
+        ninjutsu_pow: 0,
+        ninamount: -10,
+        ultEnhance: true,
+        //BUFFS
+        buff: 'Increase Critical by 20%',
+        buff2: 'Decrease Critical by 30%',
+        buff_acc: 80,
+        buff2_acc: 80,
+        image: 'https://avatarfiles.alphacoders.com/627/62714.jpg'
+    },
+    {
         name: 'Female Titan',
         nen_type: 'Transmuter',
         element: 'Water',
