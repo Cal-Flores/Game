78096b8b3731278c9626169c2d6acfadca9ee315
calebflores <calebflores1388@gmail.com>
2024-12-30 01:33:11 -0600

    blue spirit def fic

--- database/database.js
+++ database/database.js
@@ -1105,7 +1105,7 @@ const database = [
         name: 'Trunks',
         nen_type: 'Transmuter',
         element: 'Lightning',
-        elm: Lightning,
+        elm: lightning,
         hp: 3056,
         //PHYSICAL
         attack: 148,
@@ -1113,7 +1113,7 @@ const database = [
         critical: .14,
         strength: 1.25,
         //MAGIC
-        elm_attack: 180,
+        elm_attack: 190,
         elm_defense: 123,
         elm_efficiency: .40,
         potency: 1.17,
@@ -1171,7 +1171,79 @@ const database = [
         buff2: 'Decrease Critcial by 25%',
         buff_acc: 60,
         buff2_acc: 65,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl9d2ovhlR-ce_9YZVNXA5p6kTsN6SI8ZQCbEAXpL29IyVPHN23djbGNRgUgPZHVJ_IQE&usqp=CAU'
+        image: 'https://static.wikia.nocookie.net/dragonball/images/e/ef/FutureTrunksFreeTheFuture.png/revision/latest/scale-to-width-down/473?cb=20120308013904'
+    },
+    {
+        name: 'Erza',
+        nen_type: 'Conjuror',
+        element: 'Fairy',
+        elm: fairy,
+        hp: 2968,
+        //PHYSICAL
+        attack: 118,
+        defense: 145,
+        critical: .28,
+        strength: 1.12,
+        //MAGIC
+        elm_attack: 78,
+        elm_defense: 140,
+        elm_efficiency: .10,
+        potency: 1.22,
+        //WEAPON
+        weapon: 253,
+        shield: 202,
+        curse_crit: .20,
+        weaponCurse: 'Domain',
+        weapon_amount: 1,
+        //SPIRIT
+        spirit: 111,
+        spirit_def: 144,
+        spirit_crit: .10,
+        spiritCurse: 'Health',
+        spirit_amount: 55,
+        //SPEED / LUCK
+        accuracy: 6,
+        evasion: 5,
+        luck: 4,
+        stun: .05,
+        //GENJUTSU
+        genjutsu: 2,
+        gen_power: 10,
+        willpower: 6,
+        chakra: 0,
+        chakra_rec: 4,
+        drain: 4,
+        //CURSE
+        cursed_energy: 0,
+        cursed_recovery: 15,
+        negative_energy: 15,
+        cursed_tech: 'Soul-Forge - Increase Weapon by 40',
+        curse_amount: 40,
+        curse_duration: 6,
+        curse_cost: 24,
+        //DOMAIN
+        dom_purity: 6,
+        dom_lethal: 0,
+        dom_duration: 0,
+        dom_cost: 0,
+        //NINJUTSU
+        ninjutsu2: 'Requip',
+        ninJutsu2cost: 1,
+        ninJutsu2Dmg: 0,
+        nin2amount: 0,
+        ninEnhance: false,
+        //ULTIMATE
+        ninjutsu: "Holy-Hammer",
+        nin_cost: 26,
+        ninjutsu_pow: 300,
+        ninamount: 0,
+        ultEnhance: false,
+        //BUFFS
+        buff: 'Increase Willpower by 2',
+        buff2: 'Decrease Lethal by 20',
+        buff_acc: 60,
+        buff2_acc: 60,
+        image: 'https://preview.redd.it/discussion-say-something-bad-about-ezra-scarlet-v0-g1t8evn7ljzb1.jpg?width=640&crop=smart&auto=webp&s=4e72ba0f30403328c3853a8ca3d374e5b7ca8f02'
     },
     //// END /////
     /////// SUB-EMITTERS ////////
